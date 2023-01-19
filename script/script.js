ea307d906281a9d8459516b71347d02b92257f2c
Anatolyi_Drevynskyi <stiller1331@gmail.com>
2023-01-19 11:10:53 +0200

    lesson171

--- script/script.js
+++ script/script.js
@@ -11,3 +11,9 @@ let old = prompt("How old are you?", 'Age' );
 alert(`Your full name is: ${name} ${surname}, yor age is: ${old}`);
 let access = confirm('Are you admin?');
 alert(`Your full name is: ${name} ${surname}, yor age is ${old}, admin status: ${access}`);
+console.log(Boolean(alert('5')));
+console.log(true > false);
+console.log(true > 5);
+console.log(true < '5');
+console.log(true > null);
+
