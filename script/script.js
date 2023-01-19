0da0642847e40dfbfd06acb204c5edfc7128a738
Anatolyi_Drevynskyi <stiller1331@gmail.com>
2023-01-19 09:56:07 +0200

    lesson17

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+const text = 'You can ignore and double quotes like this:' ;
+const text2 = '\\\'\\\' \\\" \\\" ';
+console.log (`${text} ${text2}`);
+alert ('Please enter your data');
+let name = prompt("Enter your name", 'Name');
+alert(`Your name is: ${name}`);
+let surname = prompt("Enter your surname", 'Surname');
+alert(`Your full name is: ${name} ${surname}`);
+let old = prompt("How old are you?", 'Age' );
+alert(`Your full name is: ${name} ${surname}, yor age is: ${old}`);
+let access = confirm('Are you admin?');
+alert(`Your full name is: ${name} ${surname}, yor age is ${old}, admin status: ${access}`);
